aa859ec27d824f90c9b9b4824465259c9c32d23f
LeonanMartins <leonanjunior@hotmail.com>
2020-05-25 07:20:02 -0300

    Create Projeto.py
    Primeiro codigo que criei. Uma pequena brincadeira de perguntas e respostas, assim simulando uma inteligencia artificial.

---
+++ Projeto.py
@@ -0,0 +1,16 @@
+msg = "Olá Humano!"
+print(msg)
+nome=input('Qual o seu nome?')
+print('olá',nome,'Prazer em te conhecer!')
+peso=input('Qual é o seu peso?')
+print(peso,'?  Você precisa comer mais!')
+idade=input('Qual a sua idade?')
+print('Há muita vida ainda quando temos',idade)
+comida=input('Qual a sua comida favorita?')
+print(comida,'deve ser uma delicia')
+feita=input('Do que ela é feita?')
+print('adoro',feita,'!')
+amigo=input('Qual o nome do seu melhor amigo?')
+print('Você deve estar com saudades do',amigo,'nesse periodo de quarentena')
+
+
